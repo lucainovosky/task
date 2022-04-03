161feb0468ce6ffb22145d307fcfc6836b7a5151
Luca <luca9nta87@gmail.com>
2022-04-03 22:10:11 +0200

    style form

--- src/app/task-shared/DefaultTasks.ts
+++ src/app/task-shared/DefaultTasks.ts
@@ -4,4 +4,6 @@ export let DefaultTasks : TaskInterface[] = [
     {taskName: "Task name",  personName: "Mario Rossi", startDate: "12/01/2022", dueDate: "31/12/2022", priority: "LOW", involved: "2", state: "IN PROGRESS", progress: "20%"},
     {taskName: "Task name2", personName: "Joe Black", startDate: "12/01/2022",dueDate: "31/12/2022", priority: "LOW", involved: "2", state: "IN PROGRESS", progress: "20%"},
     {taskName: "Task name3", personName: "Antonio Bianchi", startDate: "12/01/2022",dueDate: "31/12/2022", priority: "LOW", involved: "2", state: "IN PROGRESS", progress: "20%"},
+    {taskName: "Task name4", personName: "Joe Black", startDate: "12/01/2022",dueDate: "31/12/2022", priority: "LOW", involved: "2", state: "IN PROGRESS", progress: "20%"},
+    {taskName: "Task name5", personName: "Joe Black", startDate: "12/01/2022",dueDate: "31/12/2022", priority: "LOW", involved: "2", state: "IN PROGRESS", progress: "20%"},
 ]
